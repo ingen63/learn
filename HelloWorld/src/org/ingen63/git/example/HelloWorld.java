60ba155a60b6c5f57120620ffdbe6a9fde9ca3f5
ingen63 <speckhofer@gmail.com>
2014-01-29 23:33:58 +0100

    Initial commit

---
+++ HelloWorld/src/org/ingen63/git/example/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.ingen63.git.example;
+
+/**
+ * @author Günter
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Günter");
+	}
+
+}
